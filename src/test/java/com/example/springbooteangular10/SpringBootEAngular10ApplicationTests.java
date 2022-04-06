7d72d2cfe15a30af19b1f5b929a3f4247ed656d1
Marcos Silveira <marcosrogerio1994@gmail.com>
2022-04-06 11:30:11 -0300

    first commit

---
+++ src/test/java/com/example/springbooteangular10/SpringBootEAngular10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbooteangular10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEAngular10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
